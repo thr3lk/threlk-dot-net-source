134e7bac3454fcd60284da2f48e93801a18c75e6
Justin <justin.threlkeld@gmail.com>
2024-11-21 17:41:51 -0700

    add some metadata for later use

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "threlk net",
+	url: "https://www.threlk.net",
+	language: "en",
+	description: "yet another personal website",
+	author: {
+		name: "justin",
+		email: "",
+		url: "https://www.threlk.net/about/"
+	}
+}
